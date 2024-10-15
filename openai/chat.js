d2118cc87869e71bab8f1f2c20b2e8b3c6deebf0
wjk <jk1028@g.shingu.ac.kr>
2024-10-15 11:27:42 +0900

    aaaa

--- openai/chat.js
+++ openai/chat.js
@@ -37,8 +37,53 @@ function talk(){
         listitem.className = 'list-group-item list-group-item-action list-group-item-warning';
         list.appendChild(listitem)
     }
-    ).fail(
+    ).fail(function(error){
+        console.log(error)
 
+    }
+    )
+
+}
+
+function draw(){
+    // alert(document.getElementById('item').value)
+
+    squestion = document.getElementById('item').value
+    list = document.getElementById('qlist')
+
+    listitem = document.createElement('li')
+    // alert("talk 시작")
+    data = {
+        model: smodel,
+        messages:[
+            {
+                prompt: squestion,
+                n:1,
+                size:"512x512"
+            }
+        ]
+    }
+    
+    $.ajax({
+        type:"POST",
+        url:"https://api.openai.com/v1/images/generations",
+        headers:{
+            "Authorization":"Bearer " + OPENAPI_KEY
+        },
+        data:JSON.stringify(data),
+        contentType: "application/json; charset=utf-8"
+    }
+    ).done( function(response){
+        console.log(response)
+        // alert(response.choices[0].message.content)
+        listitem.innerText = response.data[0].url
+        listitem.className = 'list-group-item list-group-item-action list-group-item-warning';
+        list.appendChild(listitem)
+    }
+    ).fail(function(error){
+        console.log(error)
+        
+    }
     )
 
 }
